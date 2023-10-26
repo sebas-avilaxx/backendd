1876369fa4a32712996947ecdc4e3a4c9837d2e7
Sebastian Avila <sebastian.avilaxx@gmail.com>
2023-10-25 21:02:53 -0300

    mocking

--- src/services/filesystem/models/courses.js
+++
@@ -1,8 +0,0 @@
-export default class Course{
-    constructor(title, description, teacherName){
-        this.title = title;
-        this.description = description;
-        this.teacherName = teacherName;
-        this.students = new Array();
-    }
-};
--- src/services/filesystem/models/students.js
+++
@@ -1,8 +0,0 @@
-export default class Student{
-    constructor(name, lastName, age){
-        this.name = name;
-        this.lastName = lastName;
-        this.age = age;
-        this.cursos = new Array();
-    }
-};
