a0f5462e307384f25717a4bb54909351b20f3348
Sebastian Avila <sebastian.avilaxx@gmail.com>
2023-11-13 21:02:42 -0300

    tercer pract

--- server/src/routes/mock.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getProducts } from "../controllers/mock.controller.js";
-
-const router = Router();
-
-router.get('/mockingproducts', getProducts);
-
-export default router;
---
+++ src/config/nodemailer.config.js
@@ -0,0 +1,9 @@
+export const mailSettings = {
+    service: 'gmail',
+    port: 587,
+    auth: {
+        user: 'coderhouseb@gmail.com',
+        pass: 'krhq zgya hoyp ctrh'
+    }
+}
+//password de la cuenta: Coder2023!
